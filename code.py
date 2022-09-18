b41f592bffdb5a5d3c2df870a2729151adb6ff5f
MrProha <mrprohagames@gmail.com>
2022-09-18 19:35:53 +0300

    homework done

--- code.py
+++ code.py
@@ -1,7 +1,19 @@
 import requests
 import bs4
-
-url = ''
-
-response = requests.get(url)
-
+from fake_headers import Headers
+headers = Headers(os="win", headers=True).generate()
+url = 'https://habr.com/ru/all/'
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+response = requests.get(url, headers=headers)
+text = response.text
+soup = bs4.BeautifulSoup(text, features="html.parser")
+articles = soup.find_all("article")
+for article in articles:
+    tags = article.find_all(class_="tm-article-snippet__hubs-item-link")
+    tags = [tag.find("span").text.lower() for tag in tags]
+    topic = article.find(class_="tm-article-snippet__title tm-article-snippet__title_h2").find("span").text
+    time = article.find(class_="tm-article-snippet__datetime-published").find("time").text
+    link = article.find(class_="tm-article-snippet__readmore").attrs["href"]
+    for key in KEYWORDS:
+        if key in tags:
+            print(f"{time} - {topic} - {link}")
